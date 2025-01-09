5aa11123cd2c3a8c22eb1d7a728c8146cda126ea
Jorik <vanveenjorik@protonmail.com>
2025-01-09 19:57:03 +0100

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::path::PathBuf;
+use std::{fs, path::Path};
 
 use clap::Parser;
 use color_eyre::{eyre::WrapErr, Result};
@@ -9,15 +9,27 @@ struct Args {
     query: String,
 }
 
-fn get_search_text(file_contents: String) -> String {
-    file_contents.lines().filter(|line| {
-        line.starts_with("Categories")
-            || line.starts_with("Exec")
-            || line.starts_with("Name")
-            || line.starts_with("GenericName")
-            || line.starts_with("Comment")
-            || line.starts_with("Keywords")
-    }).map(|line| line.split("=").nth(1).unwrap().to_lowercase()).collect()
+fn get_search_text(path: &Path, file_contents: String) -> String {
+    file_contents
+        .lines()
+        .filter(|line| {
+            line.starts_with("Categories")
+                || line.starts_with("Exec")
+                || line.starts_with("Name")
+                || line.starts_with("GenericName")
+                || line.starts_with("Comment")
+                || line.starts_with("Keywords")
+        })
+        .filter_map(|line| match line.split("=").nth(1) {
+            Some(searchable_value) => Some(searchable_value.to_lowercase()),
+            None => {
+                eprintln!(
+                    "WARING: Invalid .desktop file: {path:?}. \"{line}\" is not a valid key-value pair",
+                );
+                None
+            }
+        })
+        .collect()
 }
 
 fn main() -> color_eyre::Result<()> {
@@ -43,34 +55,54 @@ fn main() -> color_eyre::Result<()> {
             if local_share_apps.exists() {
                 dirs.push(local_share_apps);
             }
-        },
-        _ => {},
+        }
+        _ => {}
     }
 
     let matched_files: Vec<(PathBuf, String)> = dirs
         .iter()
-        .flat_map(|applications_dir| {
-            fs::read_dir(applications_dir)
-                .unwrap()
-                .filter(|file| {
-                    file.as_ref()
-                        .unwrap()
-                        .file_name()
-                        .to_str()
-                        .unwrap()
-                        .ends_with(".desktop")
-                })
-                .map(|file| {
-                    (
-                        file.as_ref().unwrap().path(),
-                        get_search_text(fs::read_to_string(file.unwrap().path()).unwrap()),
-                    )
-                })
+        .filter_map(|applications_dir| {
+            let dir_entries = match fs::read_dir(applications_dir) {
+                Ok(dir_entries) => dir_entries,
+                Err(e) => {
+                    eprintln!("WARNING: Could not list directory: {applications_dir:?}");
+                    eprintln!("{e}");
+                    return None;
+                }
+            };
+
+            Some(
+                dir_entries
+                    .filter(|file| {
+                        file.as_ref()
+                            .unwrap()
+                            .file_name()
+                            .to_str()
+                            .unwrap()
+                            .ends_with(".desktop")
+                    })
+                    .filter_map(|file| {
+                        let path = file.as_ref().unwrap().path();
+                        let contents = match fs::read_to_string(&path) {
+                            Ok(contents) => contents,
+                            Err(e) => {
+                                eprintln!("WARNING: Could not real file contents of {path:?}");
+                                eprintln!("{e}");
+                                return None;
+                            }
+                        };
+                        Some((path.clone(), get_search_text(&path, contents)))
+                    }),
+            )
         })
+        .flatten()
         .filter(|(_path, search_text)| search_text.contains(&args.query.to_lowercase()))
         .collect();
 
-    let mut matched_file_paths: Vec<String> = matched_files.into_iter().map(|(path, _)| String::from(path.to_str().unwrap())).collect();
+    let mut matched_file_paths: Vec<String> = matched_files
+        .into_iter()
+        .map(|(path, _)| String::from(path.to_str().unwrap()))
+        .collect();
     matched_file_paths.sort();
 
     println!("{}", matched_file_paths.join("\n"));
